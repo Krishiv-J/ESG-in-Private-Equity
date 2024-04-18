cb32ff31f40e7037df9f7e689a4a570b588fac11
Krishiv-J <krishivjain1234@gmail.com>
2024-04-18 19:01:45 -0400

    Stimulate and Test Data
    Updated the scripts to stimulate and test the datasets

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,47 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates the data sets 
+# Author: Krishiv Jain
+# Date: 18 April 2024
+# Contact: krishiv.jain@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: none
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
-#### Simulate data ####
-# [...ADD CODE HERE...]
+#### SIMULATE DATA ####
 
+set.seed(555) #random seed
 
+# Stimulating the dataset
 
+simulated_data <-  
+  tibble(
+    Firm = rep(1:100),    
+    Funds = round(runif(100, min = 0, max = 200)),
+    Fund_Size = runif(100, min = 100, max = 50000),
+    IRR = runif(100, min = 0, max = 100),
+    Staff_Count = round(runif(100, min = 0, max = 10000)),
+    Average_Fund_Size = Fund_Size/Funds, 
+    ESG_Score = round(runif(100, min = 0, max = 20)))
+
+#### TEST DATA ####
+
+##Check the minimum number is equal, or greater than, 0 ##
+#Based on: https://tellingstorieswithdata.com/02-drinking_from_a_fire_hose.html#simulate
+simulated_data$Funds |> min() >= 0
+simulated_data$Staff_Count |> min() >= 0
+
+##Check the class of the column is numeric##
+simulated_data$Funds |> class() == "numeric"
+simulated_data$Fund_Size |> class() == "numeric"
+simulated_data$IRR |> class() == "numeric"
+simulated_data$Staff_Count |> class() == "numeric"
+simulated_data$Average_Fund_Size |> class() == "numeric"
+simulated_data$ESG_Score |> class() == "numeric"
+
+## Check that the columns only have whole numbers ##
+#Based on: https://stackoverflow.com/q/3476782/23271634
+is.wholenumber <- function(x, tol = .Machine$double.eps^0.5) abs(x - round(x)) < tol
+is.wholenumber(simulated_data$ESG_Score)
+is.wholenumber(simulated_data$Staff_Count)
--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,36 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Testing the data set
+# Author: Krishiv Jain
+# Date: 18 April 2024
+# Contact: krishiv.jain@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Downloaded the cleaned data file from the cleaned_data folder to run these tests
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(here)
 
-#### Test data ####
+#### READ DATA ####
+cleaned_data <- read_csv(here("data/analysis_data/cleaned_data1.csv"))
+
+#### TEST DATA ####
+
+##Check the minimum number is equal, or greater than, 0 ##
+#Based on: https://tellingstorieswithdata.com/02-drinking_from_a_fire_hose.html#simulate
+cleaned_data$Totalnumberoffunds |> min() >= 0
+cleaned_data$Staff_Count |> min() >= 0
+
+##Check the class of the column is numeric##
+cleaned_data$Totalnumberoffunds |> class() == "numeric"
+cleaned_data$FundSize_Preqin |> class() == "numeric"
+cleaned_data$Sum_wirr_preqin |> class() == "numeric"
+cleaned_data$Staff_Count |> class() == "numeric"
+cleaned_data$Average_Fund_Size |> class() == "numeric"
+cleaned_data$ESG_Score |> class() == "numeric"
+
+## Check that the columns only have whole numbers ##
+#Based on: https://stackoverflow.com/q/3476782/23271634
+is.wholenumber <- function(x, tol = .Machine$double.eps^0.5) abs(x - round(x)) < tol
+is.wholenumber(cleaned_data$Staff_Count)
+is.wholenumber(cleaned_data$ESG_Score)
